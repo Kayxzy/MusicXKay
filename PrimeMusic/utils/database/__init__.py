03e281b7e34e9067a0fc07f91f8a3426350b5978
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-06-29 21:28:07 +0700

    Update __init__.py

--- PrimeMusic/utils/database/__init__.py
+++ PrimeMusic/utils/database/__init__.py
@@ -1,3 +1,5 @@
 from .db_assistant import *
 from .db_memory import *
 from .db_mongo import *
+from .assistant import (_get_assistant, get_as_names, get_assistant,
+                        save_assistant)
