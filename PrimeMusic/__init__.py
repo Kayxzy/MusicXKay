efaeee0f4fd9a194e30cdb5b847e656f9dec98ee
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-06-29 21:51:29 +0700

    Update __init__.py

--- PrimeMusic/__init__.py
+++ PrimeMusic/__init__.py
@@ -39,6 +39,38 @@ aiohttpsession = ClientSession()
 random_assistant = []
 
 
+      as status:
+        console.print("┌ [red]Booting Up The Clients...\n")
+        await app.start()
+        console.print("└ [green]Booted Bot Client")
+        console.print("\n┌ [red]Booting Up The Assistant Clients...")
+        if STRING1 != "None":
+            await ASS_CLI_1.start()
+            random_assistant.append(1)
+            console.print("├ [yellow]Booted Assistant Client")
+        if STRING2 != "None":
+            await ASS_CLI_2.start()
+            random_assistant.append(2)
+            console.print("├ [yellow]Booted Assistant Client 2")
+        if STRING3 != "None":
+            await ASS_CLI_3.start()
+            random_assistant.append(3)
+            console.print("├ [yellow]Booted Assistant Client 3")
+        if STRING4 != "None":
+            await ASS_CLI_4.start()
+            random_assistant.append(4)
+            console.print("├ [yellow]Booted Assistant Client 4")
+        if STRING5 != "None":
+            await ASS_CLI_5.start()
+            random_assistant.append(5)
+            console.print("├ [yellow]Booted Assistant Client 5")
+        if STRING6 != "None":
+            await ASS_CLI_6.start()
+            random_assistant.append(6)
+            console.print("├ [yellow]Booted Assistant Client 6")
+        console.print("└ [green]Assistant Clients Booted Successfully!")
+
+
 from .platforms import *
 
 YouTube = YouTubeAPI()
