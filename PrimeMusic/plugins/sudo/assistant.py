dff11d9c6c2f88f2646c524e974872b0d01cd3c7
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-06-29 21:39:08 +0700

    Update assistant.py

--- PrimeMusic/plugins/sudo/assistant.py
+++ PrimeMusic/plugins/sudo/assistant.py
@@ -7,7 +7,7 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineQueryResultPhoto, InputTextMessageContent,
                             Message)
 
-from PrimeMusic import ASSISTANT_PREFIX, SUDOERS, app, random_assistant
+from PrimeMusic import ASSISTANT_PREFIX, sudo, app, random_assistant
 from PrimeMusic.utils.database import get_assistant, save_assistant
 from PrimeMusic.utils.assistant import get_assistant_details
 
