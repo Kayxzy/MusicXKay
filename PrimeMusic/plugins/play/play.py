45dbd96635c586bc6925d846bf936c47738e6903
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-08-25 06:11:42 +0700

    Update play.py

--- PrimeMusic/plugins/play/play.py
+++ PrimeMusic/plugins/play/play.py
@@ -25,7 +25,7 @@ from PrimeMusic.utils.inline.play import (livestream_markup,
 from PrimeMusic.utils.inline.playlist import botplaylist_markup
 from PrimeMusic.utils.logger import play_logs
 from PrimeMusic.utils.stream.stream import stream
-from PrimeMusic.utils.sub import kay
+
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
@@ -37,7 +37,6 @@ PLAY_COMMAND = get_command("PLAY_COMMAND")
     & ~filters.edited
     & ~BANNED_USERS
 )
-@kay
 @PlayWrapper
 async def play_commnd(
     client,
